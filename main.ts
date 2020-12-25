6b58319d418e662b2e19c454360c32d3d8b08908
eschlot <eschlot@users.noreply.github.com>
2020-12-25 22:30:05 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-controller.up.onEvent(ControllerButtonEvent.Released, function () {
+function EndeSchubVertikal () {
     Rakete.ay = auftrieb
     Rakete.setImage(img`
         . . . . . . . 1 . . . . . . . . 
@@ -18,111 +18,102 @@ controller.up.onEvent(ControllerButtonEvent.Released, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `)
+}
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    EndeSchubVertikal()
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    Rakete.setImage(img`
-        . . . . . . . 1 . . . . . . . . 
-        . . 8 . . . 1 1 1 . . . 8 . . . 
-        . 8 8 8 . 1 f f f 1 . 8 8 8 . . 
-        . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
-        . 8 f f 8 1 f f f 1 8 f f 8 . . 
-        . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
-        . f 2 f 8 1 f f f 1 8 f 4 f . . 
-        f 2 2 2 f 1 1 1 1 1 f 2 4 2 f . 
-        . 2 5 2 . 1 f f f 1 . 4 2 2 . . 
-        . 5 2 4 . 1 1 1 1 1 . 2 2 2 . . 
-        . 2 4 2 . f 2 4 2 f . 2 5 5 . . 
-        . 4 2 2 . . 2 5 2 . . 5 5 5 . . 
-        . . 2 . . . 4 5 4 . . 5 2 2 . . 
-        . . . . . . . 5 . . . . 2 . . . 
-        . . . . . . . 5 . . . . . . . . 
-        . . . . . . . 5 . . . . . . . . 
-        `)
-    Rakete.ay = 0 - 3
+    if (info.life() > 0) {
+        Rakete.setImage(img`
+            . . . . . . . 1 . . . . . . . . 
+            . . 8 . . . 1 1 1 . . . 8 . . . 
+            . 8 8 8 . 1 f f f 1 . 8 8 8 . . 
+            . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
+            . 8 f f 8 1 f f f 1 8 f f 8 . . 
+            . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
+            . f 2 f 8 1 f f f 1 8 f 4 f . . 
+            f 2 2 2 f 1 1 1 1 1 f 2 4 2 f . 
+            . 2 5 2 . 1 f f f 1 . 4 2 2 . . 
+            . 5 2 4 . 1 1 1 1 1 . 2 2 2 . . 
+            . 2 4 2 . f 2 4 2 f . 2 5 5 . . 
+            . 4 2 2 . . 2 5 2 . . 5 5 5 . . 
+            . . 2 . . . 4 5 4 . . 5 2 2 . . 
+            . . . . . . . 5 . . . . 2 . . . 
+            . . . . . . . 5 . . . . . . . . 
+            . . . . . . . 5 . . . . . . . . 
+            `)
+        Rakete.ay = 0 - 3
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    Rakete.setImage(img`
-        . . . . . . . 1 . . . . . . . . 
-        . . . . . . 1 1 1 . . . . . . . 
-        . . 8 8 . 1 f f f 1 . 8 8 . . . 
-        . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
-        . 8 f 8 8 1 f f f 1 8 8 f 8 . . 
-        . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
-        . f . f 8 1 f f f 1 8 f 2 f . . 
-        . . . . f 1 1 1 1 1 . 2 2 2 . . 
-        . . . . . 1 f f f 1 . 2 2 2 . . 
-        . . . . . 1 1 1 1 1 . 2 2 2 . . 
-        . . . . . f 2 4 2 f . 2 2 2 . . 
-        . . . . . . 2 5 2 . . 5 5 5 . . 
-        . . . . . . 4 5 4 . . 5 5 5 . . 
-        . . . . . . . 5 . . . . 5 . . . 
-        . . . . . . . 5 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
-    Rakete.ax = 0 - 3
+    if (info.life() > 0) {
+        Rakete.setImage(img`
+            . . . . . . . 1 . . . . . . . . 
+            . . . . . . 1 1 1 . . . . . . . 
+            . . 8 8 . 1 f f f 1 . 8 8 . . . 
+            . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
+            . 8 f 8 8 1 f f f 1 8 8 f 8 . . 
+            . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
+            . f . f 8 1 f f f 1 8 f 2 f . . 
+            . . . . f 1 1 1 1 1 . 2 2 2 . . 
+            . . . . . 1 f f f 1 . 2 2 2 . . 
+            . . . . . 1 1 1 1 1 . 2 2 2 . . 
+            . . . . . f 2 4 2 f . 2 2 2 . . 
+            . . . . . . 2 5 2 . . 5 5 5 . . 
+            . . . . . . 4 5 4 . . 5 5 5 . . 
+            . . . . . . . 5 . . . . 5 . . . 
+            . . . . . . . 5 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        Rakete.ax = 0 - 3
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
     if (Rakete.vy > 5) {
-        game.over(false, effects.melt)
+        verlieren()
     } else {
         game.over(true, effects.confetti)
     }
 })
+function verlieren () {
+    game.over(false, effects.dissolve)
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.shrub, function (sprite, location) {
-    game.over(false, effects.melt)
+    verlieren()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.oceanDepths1, function (sprite, location) {
-    game.over(false, effects.melt)
+    verlieren()
 })
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    Rakete.ax = idle
-    Rakete.setImage(img`
-        . . . . . . . 1 . . . . . . . . 
-        . . 8 . . . 1 1 1 . . . 8 . . . 
-        . . 8 . . 1 f f f 1 . . 8 . . . 
-        . 8 8 8 . 1 1 1 1 1 . 8 8 8 . . 
-        . 8 f 8 8 1 f f f 1 8 8 f 8 . . 
-        . 8 8 8 . 1 1 1 1 1 . 8 8 8 . . 
-        . f . f . 1 f f f 1 . f . f . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . 1 f f f 1 . . . . . . 
-        . . . . . 1 1 1 1 1 . . . . . . 
-        . . . . . f . . . f . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)
+    endeSchubHorizontal()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.rock0, function (sprite, location) {
-    game.over(false, effects.melt)
+    verlieren()
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    Rakete.setImage(img`
-        . . . . . . . 1 . . . . . . . . 
-        . . . . . . 1 1 1 . . . . . . . 
-        . . 8 8 . 1 f f f 1 . 8 8 . . . 
-        . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
-        . 8 8 8 8 1 f f f 1 8 8 8 8 . . 
-        . 8 f 8 8 1 1 1 1 1 8 8 f 8 . . 
-        . f 2 f 8 1 f f f 1 8 f 4 f . . 
-        f 2 2 2 f 1 1 1 1 1 f 2 4 2 . . 
-        . 2 2 2 . 1 f f f 1 . 4 2 . . . 
-        . 2 5 2 . 1 1 1 1 1 . . . . . . 
-        . 2 5 2 . f 2 4 2 f . . . . . . 
-        . 5 5 5 . . 2 5 2 . . . . . . . 
-        . 5 5 5 . . 4 5 4 . . . . . . . 
-        . . . . . . . 5 . . . . . . . . 
-        . . . . . . . 5 . . . . . . . . 
-        . . . . . . . 5 . . . . . . . . 
-        `)
-    Rakete.ax = 0 + 3
+    if (info.life() > 0) {
+        Rakete.setImage(img`
+            . . . . . . . 1 . . . . . . . . 
+            . . . . . . 1 1 1 . . . . . . . 
+            . . 8 8 . 1 f f f 1 . 8 8 . . . 
+            . 8 8 8 8 1 1 1 1 1 8 8 8 8 . . 
+            . 8 8 8 8 1 f f f 1 8 8 8 8 . . 
+            . 8 f 8 8 1 1 1 1 1 8 8 f 8 . . 
+            . f 2 f 8 1 f f f 1 8 f 4 f . . 
+            f 2 2 2 f 1 1 1 1 1 f 2 4 2 . . 
+            . 2 2 2 . 1 f f f 1 . 4 2 . . . 
+            . 2 5 2 . 1 1 1 1 1 . . . . . . 
+            . 2 5 2 . f 2 4 2 f . . . . . . 
+            . 5 5 5 . . 2 5 2 . . . . . . . 
+            . 5 5 5 . . 4 5 4 . . . . . . . 
+            . . . . . . . 5 . . . . . . . . 
+            . . . . . . . 5 . . . . . . . . 
+            . . . . . . . 5 . . . . . . . . 
+            `)
+        Rakete.ax = 0 + 3
+    }
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral0, function (sprite, location) {
-    game.over(false, effects.melt)
-})
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
+function endeSchubHorizontal () {
     Rakete.ax = idle
     Rakete.setImage(img`
         . . . . . . . 1 . . . . . . . . 
@@ -142,13 +133,23 @@ controller.right.onEvent(ControllerButtonEvent.Released, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `)
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral0, function (sprite, location) {
+    verlieren()
+})
+info.onLifeZero(function () {
+    Rakete.ax = 0
+    Rakete.ay = auftrieb
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    endeSchubHorizontal()
 })
 let Rakete: Sprite = null
 let idle = 0
 let auftrieb = 0
 auftrieb = 2
 idle = 0
-info.setLife(500)
+info.setLife(1000)
 Rakete = sprites.create(img`
     . . . . . . . 1 . . . . . . . . 
     . . 8 . . . 1 1 1 . . . 8 . . . 
